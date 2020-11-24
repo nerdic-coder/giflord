5c63e9ac239ccf1783aa93de4a79c395c34ce3e8
TheNerdicCoder <johan@efdapps.com>
2020-11-24 20:57:57 +0200

    Add Ionic + Vue

---
+++ src/views/Home.vue
@@ -0,0 +1,71 @@
+<template>
+  <ion-page>
+    <ion-header color="dark" :translucent="true">
+      <ion-toolbar color="dark">
+        <ion-title>GIF Lord</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    
+    <ion-content color="dark" :fullscreen="true">
+      <ion-header color="dark" collapse="condense">
+        <ion-toolbar color="dark">
+          <ion-title size="large">GIF Lord</ion-title>
+        </ion-toolbar>
+      </ion-header>
+    
+      <div id="container">
+        <img src="assets/lord.gif" alt="Who? GIF Lord!" />
+      </div>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script lang="ts">
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/vue';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'Home',
+  components: {
+    IonContent,
+    IonHeader,
+    IonPage,
+    IonTitle,
+    IonToolbar
+  }
+});
+</script>
+
+<style scoped>
+#container {
+  text-align: center;
+  
+  position: absolute;
+  left: 0;
+  right: 0;
+  top: 50%;
+  transform: translateY(-50%);
+}
+
+#container strong {
+  font-size: 20px;
+  line-height: 26px;
+}
+
+#container p {
+  font-size: 16px;
+  line-height: 22px;
+  
+  color: #8c8c8c;
+  
+  margin: 0;
+}
+
+#container a {
+  text-decoration: none;
+}
+
+img {
+  width: 100%;
+}
+</style>
